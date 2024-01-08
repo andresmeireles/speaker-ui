a651f01fd9d97c4d090a7c0f1e9f660dec407af9
André Meireles <andre2meireles@gmail.com>
2024-01-08 16:23:10 -0300

    show api path on login

--- src/routes/(unprotected)/login/+page.svelte
+++ src/routes/(unprotected)/login/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { PUBLIC_API_URL } from '$env/static/public';
 	import { triggerToastMessage } from '$lib/actions/toast';
 	import type { ActionData } from './$types';
 	import Confirm from './components/Confirm.svelte';
@@ -29,8 +30,14 @@
 	}
 </script>
 
-{#if insertCodePhase}
-	<Confirm {email} {form} />
-{:else}
-	<Login />
-{/if}
+<div class="w-full h-full">
+	{#if insertCodePhase}
+		<Confirm {email} {form} />
+	{:else}
+		<Login />
+	{/if}
+	<div class="justify-center text-center mt-12">
+		<div>api</div>
+		<pre>{PUBLIC_API_URL}</pre>
+	</div>
+</div>
--- src/routes/(unprotected)/login/components/Confirm.svelte
+++ src/routes/(unprotected)/login/components/Confirm.svelte
@@ -6,27 +6,25 @@
 	export let form: ActionData;
 </script>
 
-<div class="w-full h-full">
-	<div class="flex justify-center">
-		<div class="bg-slate-200 w-4/5 p-5 rounded-md mt-24 border-black border">
-			<h1 class="text-3xl w-full text-center">Confirme o código</h1>
-			<p class="w-full text-center text-sm">Um código foi enviado para seu email</p>
-			<div class="py-4">
-				<form action="?/confirm" method="POST" use:enhance>
-					<input type="hidden" name="email" bind:value={email} />
-					<input
-						type="text"
-						name="code"
-						placeholder="Adicionar código"
-						class="w-full p-2 mr-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-						value={form?.values?.code ?? ''}
-					/>
-					<label for="stay_connected">
-						<input type="checkbox" name="stay_connected" id="stay_connected" />Manter conectado</label
-					>
-					<button class="rounded-md bg-blue-500 text-white p-2 mt-4 w-full">Confimar</button>
-				</form>
-			</div>
+<div class="flex justify-center">
+	<div class="bg-slate-200 w-4/5 p-5 rounded-md mt-24 border-black border">
+		<h1 class="text-3xl w-full text-center">Confirme o código</h1>
+		<p class="w-full text-center text-sm">Um código foi enviado para seu email</p>
+		<div class="py-4">
+			<form action="?/confirm" method="POST" use:enhance>
+				<input type="hidden" name="email" bind:value={email} />
+				<input
+					type="text"
+					name="code"
+					placeholder="Adicionar código"
+					class="w-full p-2 mr-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+					value={form?.values?.code ?? ''}
+				/>
+				<label for="stay_connected">
+					<input type="checkbox" name="stay_connected" id="stay_connected" />Manter conectado</label
+				>
+				<button class="rounded-md bg-blue-500 text-white p-2 mt-4 w-full">Confimar</button>
+			</form>
 		</div>
 	</div>
 </div>
